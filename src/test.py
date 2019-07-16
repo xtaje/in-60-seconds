d33e2784cd3bcf71402cc030fe03e5e5b922e27d
Ed Jung <xtaje@users.noreply.github.com>
2019-07-16 00:08:16 -0700

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,9 @@
+def is_blacklisted(key, text):
+    words = text.split()
+    limit = THRESHOLD * len(words)
+    for w in words:
+        if w in STOCK_SYMBOLS:
+           limit -=1
+
+        if limit <= 0:
+            yield key
